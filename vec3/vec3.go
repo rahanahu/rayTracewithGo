9c9e84800d79c68c84302341e42ff92a9809d9b2
rahanahu <rahanahu@gmail.com>
2022-04-30 04:26:31 +0900

    add vec3 module

--- vec3/vec3.go
+++ vec3/vec3.go
@@ -1,15 +1,33 @@
 package vec3
 
+import "math"
+
 type Vec3 struct {
 	X float64
 	Y float64
 	Z float64
 }
 
+// type Point3  Vec3
+// type Color  Vec3
+
 func (a *Vec3) Plus(v *Vec3) *Vec3 {
 	a.X += v.X
 	a.Y += v.Y
 	a.Z += v.Z
+	return a
+}
+func (a *Vec3) Minus(v *Vec3) *Vec3 {
+	a.X -= v.X
+	a.Y -= v.Y
+	a.Z -= v.Z
+	return a
+}
+
+func (a *Vec3) Times(v *Vec3) *Vec3 {
+	a.X *= v.X
+	a.Y *= v.Y
+	a.Z *= v.Z
 	return a
 }
 
@@ -22,4 +40,30 @@ func (a *Vec3) Scale(f float64) *Vec3 {
 	a.Y = f * a.Y
 	a.Z = f * a.Z
 	return a
+}
+
+func (a *Vec3) LengthSquared() float64 {
+	return a.X*a.X + a.Y*a.Y + a.Z*a.Z
+}
+
+func (a *Vec3) Length() float64 {
+	return math.Sqrt(a.LengthSquared())
+}
+
+func (a *Vec3) Dot() float64 {
+	return a.LengthSquared()
+}
+
+func (a *Vec3) Cross(t *Vec3) *Vec3 {
+	x := a.Y*t.Z - a.Z*t.Y
+	y := a.Z*t.X - a.X*t.Z
+	z := a.X*t.Y - a.Y*t.X
+	a.X = x
+	a.Y = y
+	a.Z = z
+	return a
+}
+
+func (a *Vec3) UnitVector() *Vec3 {
+	return a.Scale(1 / a.Length())
 }
--- vec3/vec3_test.go
+++ vec3/vec3_test.go
@@ -6,8 +6,10 @@ import (
 	"testing"
 )
 
+const d float64 = 0.00000001
+
 func TestVec3_Plus(t *testing.T) {
-	const d float64 = 0.00000001
+
 	type fields struct {
 		X float64
 		Y float64
@@ -79,7 +81,6 @@ func TestVec3_Inverse(t *testing.T) {
 }
 
 func TestVec3_Scale(t *testing.T) {
-	const d float64 = 0.00000001
 	type fields struct {
 		X float64
 		Y float64
@@ -115,6 +116,271 @@ func TestVec3_Scale(t *testing.T) {
 				t.Errorf("Vec3.Scale() = %v, want %v", got, tt.want)
 			}
 
+		})
+	}
+}
+
+func TestVec3_Minus(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	type args struct {
+		v *Vec3
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Vec3
+	}{
+		{
+			name:   "sub two vector",
+			fields: fields{0.1, 0.2, 0.5},
+			args:   args{&Vec3{0.1, 0.3, 0.6}},
+			want:   &Vec3{0, -0.1, -0.1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.Minus(tt.args.v)
+			if !((math.Abs(got.X-tt.want.X) < d) &&
+				(math.Abs(got.Y-tt.want.Y) < d) &&
+				(math.Abs(got.Z-tt.want.Z) < d)) {
+				t.Errorf("Vec3.Minus() = %v, want %v", got, tt.want)
+			}
+
+		})
+	}
+}
+
+func TestVec3_LengthSquared(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		{
+			name:   "square length",
+			fields: fields{3, 4, 5},
+			want:   50,
+		},
+		{
+			name:   "square length",
+			fields: fields{3.1, 4.5, 5.3},
+			want:   57.95,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.LengthSquared()
+			if !(math.Abs(got-tt.want) < d) {
+				t.Errorf("Vec3.LengthSquared() = %v, want %v", got, tt.want)
+			}
+
+		})
+	}
+}
+
+func TestVec3_Length(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		{
+			name:   "square length",
+			fields: fields{3, 4, 5},
+			want:   math.Sqrt(50),
+		},
+		{
+			name:   "square length",
+			fields: fields{3.1, 4.5, 5.3},
+			want:   math.Sqrt(57.95),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.Length()
+			if !(math.Abs(got-tt.want) < d) {
+				t.Errorf("Vec3.Length() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVec3_Times(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	type args struct {
+		v *Vec3
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Vec3
+	}{
+		{
+			name:   "multiple two vector",
+			fields: fields{0.1, 0.2, 0.5},
+			args:   args{&Vec3{0.1, 0.3, 0.6}},
+			want:   &Vec3{0.01, 0.06, 0.3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.Times(tt.args.v)
+			if !((math.Abs(got.X-tt.want.X) < d) &&
+				(math.Abs(got.Y-tt.want.Y) < d) &&
+				(math.Abs(got.Z-tt.want.Z) < d)) {
+				t.Errorf("Vec3.Times() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVec3_Dot(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		{
+			name:   "Dot1",
+			fields: fields{3, 4, 5},
+			want:   50,
+		},
+		{
+			name:   "Dot2",
+			fields: fields{3.1, 4.5, 5.3},
+			want:   57.95,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.Dot()
+			if !(math.Abs(got-tt.want) < d) {
+				t.Errorf("Vec3.Dot() = %v, want %v", got, tt.want)
+			}
+
+		})
+	}
+}
+
+func TestVec3_Cross(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	type args struct {
+		t *Vec3
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Vec3
+	}{
+		{
+			name:   "cross two vector",
+			fields: fields{3, 4, 1},
+			args:   args{&Vec3{3, 7, 5}},
+			want:   &Vec3{13, -12, 9},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.Cross(tt.args.t)
+			if !((math.Abs(got.X-tt.want.X) < d) &&
+				(math.Abs(got.Y-tt.want.Y) < d) &&
+				(math.Abs(got.Z-tt.want.Z) < d)) {
+				t.Errorf("Vec3.Cross() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVec3_UnitVector(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   *Vec3
+	}{
+		{
+			name:   "unit vector 1",
+			fields: fields{1, 2, 3},
+			want:   &Vec3{1 / 3.741657386774, 2 / 3.741657386774, 3 / 3.741657386774},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &Vec3{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			got := a.UnitVector()
+			if !((math.Abs(got.X-tt.want.X) < d) &&
+				(math.Abs(got.Y-tt.want.Y) < d) &&
+				(math.Abs(got.Z-tt.want.Z) < d)) {
+				t.Errorf("Vec3.UnitVector() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
